d57884cb924980e66c66273da15a83c113fa72ed
papahgilak05 <papahgilak@gmail.com>
2022-04-04 13:12:44 +0700

    Main

---
+++ Client/Controllers/LatihanController.cs
@@ -0,0 +1,31 @@
+﻿using Client.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Controllers
+{
+    public class LatihanController : Controller
+    {
+        private readonly ILogger<LatihanController> _logger;
+
+        public LatihanController(ILogger<LatihanController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Gilang()
+        {
+            return View();
+        }
+    }
+}
--- Client/wwwroot/js/site.js
+++ Client/wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const animals = [
+    { name: "garfield", species: "cat", class: { name: "mamalia" } },
+    { name: "nemo", species: "fish", class: { name: "invertebrata" } },
+    { name: "tom", species: "cat", class: { name: "mamalia" } },
+    { name: "gary", species: "cat", class: { name: "mamalia" } },
+    { name: "dory", species: "fish", class: { name: "invertebrata" } }
+]
+console.log(animals);
+
+const OnlyCat = [];
+
+
+for (var i = 0; i < animals.length; i++)
+{
+    if (animals[i].species == "cat") {
+        OnlyCat.push(animals[i]);
+    }
+    else if (animals[i].species == "fish") {
+        animals[i].class.name = "Non-Mamalia";
+    }
+
+}
+console.log(OnlyCat);
+
+
